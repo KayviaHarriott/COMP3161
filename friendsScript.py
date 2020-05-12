416b26194216312ac5153dd936cb8119a286104b
kayharriott <kayviaharriott@gmail.com>
2020-05-11 20:09:08 -0500

    Updated sql

--- friendsScript.py
+++ friendsScript.py
@@ -1,19 +1,19 @@
 import random
+import secrets
 
 def writetofile():
     f = open("DBProject.sql", "a")
 
+    friendtypes = ["Relative", "Work", "School"]
+
     for i in range(100):
-        f.write("Insert into friend VALUES (%d,%d);\r" % ( i+1, random.randint(1,21)))
-        f.write("Insert into friend VALUES (%d,%d);\r" % ( i+1, random.randint(1,21)))  
+
+        id = i + 1
+        ftype = secrets.choice(friendtypes)
+        rand1 = random.randint(1,100)
+
+        f.write("Insert into friend VALUES (%d,%d,\"%s\");\r" % ( id, rand1,ftype))
+        f.write("Insert into friend VALUES (%d,%d,\"%s\");\r" % ( rand1, id,ftype)) 
     f.close()
 
 writetofile()
-
-#to run from terminal 
-###Gonna run thi, ye ws from here, or does someone know how to run from terminal? 
-###I ran from my laptop and just copied and pasted but wanna run from here to ensure it works
-
-##SHould be the same way. just run the file location ye with that 
-#got it oh crazy, i can see what you typing in terminal, nice go ahead
-#nah, it ran. check dbproject the output is there .yeah it worked ^_^
